11e2dd60a105da9790f95fcfd28761c42f4fafb7
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2022-12-18 16:26:59 +0100

    Add few function
    add checkDraw to check is a draw is happening
    add checkWin to check if the values are returning a Win
    encounterd bug. At the momment you cant lose.

--- script.js
+++ script.js
@@ -4,7 +4,46 @@ function getComputerChoice () {
     return CHOICES[Math.floor((Math.random() * CHOICES.length))];
 };
 
-function playRound (playerSelection, computerSelection) {
+function caseInsensitive (playerSelection) {
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+}
+/* !!!!!!!!!!!!!! BUG AREA !!!!!!!!!!!!!!!! */
+function checkWin (playerSelection, computerSelection) {
+    if ((playerSelection != computerSelection) && checkDraw(playerSelection,computerSelection) == false) {
+        return true;  
+    } else {
+        return false;
+    }
 };
 
-console.log(getComputerChoice());
+function checkDraw (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function playRound (playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = caseInsensitive(playerSelection);
+
+    console.log('Computer: ' + computerSelection);
+    console.log('Player: ' + playerSelection);
+
+    if (checkDraw(playerSelection, computerSelection)) {
+        console.log (`It's a Draw! ${playerSelection} and ${computerSelection}`);
+    } else if (checkWin(playerSelection, computerSelection) === true) {
+        console.log (`You Win! ${playerSelection} beats ${computerSelection}`);
+    } else {
+        console.log (`You Los! ${computerSelection} beats ${playerSelection}`);
+    }
+};
+
+/* !!!!!!!!!!!!!! BUG AREA !!!!!!!!!!!!!!!! 
+    Doesnt return loss!
+*/
+
+playRound('rock');
+
+
