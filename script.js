fffc6d9dc8b1a874dff902bc5beb007761c2c80f
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2022-12-19 02:10:54 +0100

    Bugfix logi error in checkWin function

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ function getComputerChoice () {
 function caseInsensitive (playerSelection) {
     return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 }
-/* !!!!!!!!!!!!!! BUG AREA !!!!!!!!!!!!!!!! */
+
 function checkWin (playerSelection, computerSelection) {
-    if ((playerSelection != computerSelection) && checkDraw(playerSelection,computerSelection) == false) {
+    if (!checkDraw(playerSelection,computerSelection) 
+    && ((playerSelection == 'Rock' && computerSelection == 'Sicssors')
+    || (playerSelection == 'Scissors' && computerSelection == 'Paper')
+    || (playerSelection == 'Paper' && computerSelection == 'Rock'))) {
         return true;  
     } else {
         return false;
@@ -22,28 +25,21 @@ function checkDraw (playerSelection, computerSelection) {
     } else {
         return false;
     }
-}
+};
 
 function playRound (playerSelection, computerSelection) {
     computerSelection = getComputerChoice();
     playerSelection = caseInsensitive(playerSelection);
 
-    console.log('Computer: ' + computerSelection);
-    console.log('Player: ' + playerSelection);
-
     if (checkDraw(playerSelection, computerSelection)) {
         console.log (`It's a Draw! ${playerSelection} and ${computerSelection}`);
-    } else if (checkWin(playerSelection, computerSelection) === true) {
+    } else if (checkWin(playerSelection, computerSelection)) {
         console.log (`You Win! ${playerSelection} beats ${computerSelection}`);
     } else {
-        console.log (`You Los! ${computerSelection} beats ${playerSelection}`);
+        console.log (`You Lose! ${computerSelection} beats ${playerSelection}`);
     }
 };
 
-/* !!!!!!!!!!!!!! BUG AREA !!!!!!!!!!!!!!!! 
-    Doesnt return loss!
-*/
-
-playRound('rock');
+playRound('Paper');
 
 
