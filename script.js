9dcf74b2ae31ed5159262b7f8a26970f5daf43fa
GWANDO.code <118937578+GWANDOcode@users.noreply.github.com>
2023-01-10 18:08:35 +0100

    Add ui
    - Simple UI with 4 Buttons (ROCK, PAPER, SICCORS, RESET).
    - Add function checkScore for end results.
    - Add eventlisteners to end results, to make the result text act as a reset
    button that switches text when you hover it.
    - Made the game hide and an result screen appear.

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const CHOICES = ['Rock', 'Paper', 'Sicssors'];
 let result;
 let scorePlayer = 0;
 let scoreComputer = 0;
+let endResult;
+const maxScore = 5;
+
+const gameDiv = document.querySelector("#gameDiv");
+const resultDiv = document.querySelector("#resultDiv");
+resultDiv.style.display = "none";
+
+const h2 = document.querySelector("h2");
+
+const scorePlayerDisplay = document.querySelector("#scorePlayerDisplay");
+const scoreComputerDisplay = document.querySelector("#scoreComputerDisplay");
+const computerSelectionDisplay = document.querySelector(".computerSelectionDisplay")
+scorePlayerDisplay.textContent = `Your Score: ${scorePlayer}`;
+scoreComputerDisplay.textContent = `Computer: ${scoreComputer}`;
 
 //Randomized computer choices
 function getComputerChoice () {
@@ -35,37 +49,80 @@ function checkDraw (playerSelection, computerSelection) {
 };
 
 //function to play 1 round
-function playRound (playerSelection, computerSelection = getComputerChoice()) {
+function playRound (playerSelection, computerSelection) {
     playerSelection = caseInsensitive(playerSelection);
 
     if (checkDraw(playerSelection, computerSelection)) {
-        result = `It's a Draw! ${playerSelection} and ${computerSelection}`;
+        result = `It's a Draw! You chose ${playerSelection} and the computer chose ${computerSelection}`;
     } else if (checkWin(playerSelection, computerSelection)) {
-        result = `You Win! ${playerSelection} beats ${computerSelection}`;
+        result = `You Win! Your choice ${playerSelection} beats computers choice ${computerSelection}`;
         scorePlayer++;
     } else {
-        result =`You Lose! ${computerSelection} beats ${playerSelection}`;
+        result =`You Lose! Computers choice ${computerSelection} beats your choice ${playerSelection}`;
         scoreComputer++;
-    }
+    };
+
+    checkScore();
+
+    scorePlayerDisplay.textContent = `Your Score: ${scorePlayer}`;
+    scoreComputerDisplay.textContent = `Computer: ${scoreComputer}`;
+    computerSelectionDisplay.textContent = result;
 };
 
 //resets score
 function resetScore () {
     scorePlayer = 0;
     scoreComputer = 0;
+    scorePlayerDisplay.textContent = `Your Score: ${scorePlayer}`;
+    scoreComputerDisplay.textContent = `Computer: ${scoreComputer}`;
+    result=``;
+    computerSelectionDisplay.textContent = result;
+
 };
 
-//g
-function game() {
-    playerSelection = prompt('Whats your choice?');
-    console.log(`Round: ${round + 1}`);
-    playRound (playerSelection);
-    console.log(result);
-    console.log(`Your Score: ${scorePlayer} Computer Score: ${scoreComputer}`);
+const resetsBtn = document.querySelector("#resetBtn");
+resetsBtn.addEventListener("click", resetScore);
 
-    resetScore ();
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn")
+
+rockBtn.addEventListener("click", () => handleClick("rock"));
+paperBtn.addEventListener("click", () => handleClick("paper"));
+scissorsBtn.addEventListener("click", () => handleClick("scissors"));
+
+
+function handleClick (playerSelection) {
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+}
+
+function checkScore() {
+    if(scorePlayer === maxScore) {
+        gameDiv.classList.toggle("hide");
+        endResult = "YOU WON!"
+
+        resultDiv.style.display = "flex";
+        h2.textContent = endResult
+    } else if (scoreComputer === maxScore) {
+        gameDiv.classList.toggle("hide");
+        endResult = "YOU LOST!"
+
+        resultDiv.style.display = "flex";
+        h2.textContent = endResult
+    }
 };
 
-//game();
+h2.addEventListener("click", () => {
+    gameDiv.classList.toggle("hide");
+    resultDiv.style.display = "none";
+    resetScore;
+});
 
+h2.addEventListener("mouseover", () => {
+    h2.textContent = "RESET?"
+});
 
+h2.addEventListener("mouseout", () => {
+    h2.textContent = endResult;
+});
